fb9c342afa2fd60bdbe27080d95e942425dfe7b0
Jerome Heymonet <jerome@heymonet.com>
2012-09-03 23:41:44 +0200

    Display primary interface info in a sub menu
    Display primary interface info in a sub menu
    Add separator
    Log selected interface

--- NetworkSwitcher/NetworkSwitcher/AppDelegate.m
+++ NetworkSwitcher/NetworkSwitcher/AppDelegate.m
@@ -31,7 +31,7 @@
     statusItemView = [[StatusItemView alloc] initWithStatusItem:statusItem];
     statusItemView.action = @selector(displayMenu);
     statusItemView.target = self;
-    
+
     [statusItemView setImage:[NSImage imageNamed:@"Status.png"]];
     [statusItemView setAlternateImage:[NSImage imageNamed:@"StatusHighlighted.png"]];
 }
@@ -47,7 +47,8 @@
 
 -(IBAction)clickedItem:(NSMenuItem *)sender {
     NSLog(@"Sender %@ %ld",sender, sender.tag);
-    
+    NetworkInterface *aInterface = [[self getSortedInterfaceByPrimaryService:[[self getPrimaryInterfaceInfo] objectForKey:@"PrimaryService"] forInterface:[self getAllInterfaces]] objectAtIndex:sender.tag];
+    NSLog(@"Interface %@",aInterface);
 }
 
 -(IBAction)quit:(id)sender {
@@ -95,14 +96,59 @@
         interface.interfaceSystemName = (__bridge NSString *)SCNetworkInterfaceGetBSDName(networkInterface);
         interface.interfaceType = (__bridge NSString *)SCNetworkInterfaceGetInterfaceType(networkInterface);
         
-        if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeEthernet]){
+        /* 
+         Existing interface type in the framework
+         const CFStringRef kSCNetworkInterfaceType6to4;
+         const CFStringRef kSCNetworkInterfaceTypeBluetooth;
+         const CFStringRef kSCNetworkInterfaceTypeBond;
+         const CFStringRef kSCNetworkInterfaceTypeEthernet;
+         const CFStringRef kSCNetworkInterfaceTypeFireWire;
+         const CFStringRef kSCNetworkInterfaceTypeIEEE80211;
+         const CFStringRef kSCNetworkInterfaceTypeIPSec;
+         const CFStringRef kSCNetworkInterfaceTypeIrDA;
+         const CFStringRef kSCNetworkInterfaceTypeL2TP;
+         const CFStringRef kSCNetworkInterfaceTypeModem;
+         const CFStringRef kSCNetworkInterfaceTypePPP;
+         const CFStringRef kSCNetworkInterfaceTypePPTP;
+         const CFStringRef kSCNetworkInterfaceTypeSerial;
+         const CFStringRef kSCNetworkInterfaceTypeVLAN;
+         const CFStringRef kSCNetworkInterfaceTypeWWAN;
+         const CFStringRef kSCNetworkInterfaceTypeIPv4;
+         */
+        
+        
+        if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeBluetooth]){
+            interface.interfaceImageName = @"Bluetooth.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeBond]){
+            interface.interfaceImageName = @"Bond.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeEthernet]){
             interface.interfaceImageName = @"Ethernet.png";
         } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeFireWire]){
             interface.interfaceImageName = @"Firewire.png";
-        }  else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeBluetooth]){
-            interface.interfaceImageName = @"Bluetooth.png";
-        }  else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeIEEE80211]){
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeIEEE80211]){
             interface.interfaceImageName = @"Wireless.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeIPSec]){
+            interface.interfaceImageName = @"IPSec.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeIrDA]){
+            interface.interfaceImageName = @"IrDA.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeL2TP]){
+            interface.interfaceImageName = @"L2TP.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeModem]){
+            interface.interfaceImageName = @"Modem.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypePPP]){
+            interface.interfaceImageName = @"PPP.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypePPTP]){
+            interface.interfaceImageName = @"PPTP.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeSerial]){
+            interface.interfaceImageName = @"Serial.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeVLAN]){
+            interface.interfaceImageName = @"VLAN.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeWWAN]){
+            interface.interfaceImageName = @"WWAN.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeIPv4]){
+            interface.interfaceImageName = @"IPv4.png";
+        } else if([interface.interfaceType isEqualToString:(NSString *)kSCNetworkInterfaceTypeIPSec]){
+            interface.interfaceImageName = @"IPSec.png";
         }
         
         [allInts addObject:interface];
@@ -182,13 +228,26 @@
 
 -(void) setMenuItemsWithArray:(NSArray *) content {
     [aMenu removeAllItems];
+    [aMenu addItem:[[NSMenuItem alloc] initWithTitle:@"Set primary interface" action:nil keyEquivalent:@""]];
+    
+    [aMenu addItem:[NSMenuItem separatorItem]];
+    NSDictionary *primaryInfo = [self getPrimaryInterfaceInfo];
+    NSString *primaryService = [primaryInfo objectForKey:@"PrimaryService"];
     for (NetworkInterface *aInterface in content){
         NSMenuItem *aMenuItem = [[NSMenuItem alloc] initWithTitle:aInterface.interfaceName action:@selector(clickedItem:) keyEquivalent:@""];
         [aMenuItem setTag:[content indexOfObject:aInterface]];
         [aMenuItem setImage:[NSImage imageNamed:aInterface.interfaceImageName]];
+        if([aInterface.serviceID isEqualToString:primaryService]) {
+            NSMenu *subMenu = [[NSMenu alloc] initWithTitle:@"Sub menu"];
+            [subMenu addItem:[[NSMenuItem alloc] initWithTitle:aInterface.hardwareAddress action:nil keyEquivalent:@""]];
+            [subMenu addItem:[[NSMenuItem alloc] initWithTitle:(__bridge NSString*)[self getPrimaryAddress] action:nil keyEquivalent:@""]];
+            [aMenu setSubmenu:subMenu forItem:aMenuItem];
+        }
+        
         [aMenu addItem:aMenuItem];
     }
-    [aMenu addItemWithTitle:@"Quit" action:@selector(quit:) keyEquivalent:@"Q"];
+    [aMenu addItem:[NSMenuItem separatorItem]];
+    [aMenu addItemWithTitle:@"Quit" action:@selector(quit:) keyEquivalent:@""];
 }
 
 @end
